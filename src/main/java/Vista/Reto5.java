28fe54c617afbcf8c95fee01477e658b41be5d35
Jhenox86 <jhenox@gmail.com>
2022-08-12 00:26:21 -0500

    Se agrega el mensaje en el main

--- src/main/java/Vista/Reto5.java
+++ src/main/java/Vista/Reto5.java
@@ -24,7 +24,7 @@ public class Reto5 {
          reportesView.setSize(600, 400);
          reportesView.setVisible(true);
          reportesView.setResizable(false);
-         reportesView.setTitle("Informes - Reto 5");
+         reportesView.setTitle("Informes - Reto 5 Ciclo 2");
          reportesView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          reportesView.setLocationRelativeTo(null);      //ubica la ventana en el centro de la pantalla 
     }
